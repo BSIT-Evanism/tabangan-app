2a6248f929641ebddf6663e720dcfdb114a62597
BSIT-Evanism <johnevanporcioncula.solanoy@bicol-u.edu.ph>
2023-10-18 01:18:10 +0800

    initial test

---
+++ app/not-found.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function notfound() {
+    return (
+        <div>Maybe this is not the page you're looking for</div>
+    )
+}
+
+export default notfound
--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/404',
+                destination: '/not-found',
+                permanent: true,
+            },
+        ]
+    },
+    async rewrites() {
+        return [
+            {
+                source: '/not-found',
+                destination: '/404',
+            },
+        ]
+    },
+
+}
 
 module.exports = nextConfig
