7ddd054cbc5727da1e26249e9fe983a40c38c6d8
BSIT-Evanism <johnevanporcioncula.solanoy@bicol-u.edu.ph>
2023-10-18 03:00:36 +0800

    qwik

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,8 +1,16 @@
+import Link from 'next/link'
 import React from 'react'
 
 function notfound() {
     return (
-        <div>Maybe this is not the page youre looking for</div>
+        <div className='container flex justify-center items-center'>
+            <div className='w-full h-52 flex flex-col justify-center items-center'>
+                <h1 className='text-5xl font-bold text-center p-10'>Maybe this is not the page you&apos;re looking for</h1>
+                <Link href='/'>
+                    <button className='bg-gray-200 p-7 rounded-lg'>Go back to home</button>
+                </Link>
+            </div>
+        </div>
     )
 }
 
